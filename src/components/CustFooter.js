202b2296ac514bb7bffb4e91ff1f2ffe5848979d
Rizkia-AS <rizkia.as.ac@gmail.com>
2021-10-12 09:31:08 +0900

    Updates

--- src/components/CustFooter.js
+++
@@ -1,9 +0,0 @@
-const CustFooter = ({innerText}) => {
-    return (
-        <footer className="custom-footer">
-            <h6>{innerText}</h6>
-        </footer>
-    )
-}
-
-export default CustFooter
